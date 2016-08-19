22333f190e84cc7a38b38275cbccaa442bbcd929
Hyunkyu Yoon <hkyoon85@gmail.com>
2016-08-19 18:21:35 +0900

    Commit starter files

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { GoogleMapLoader, GoogleMap, OverlayView, Marker, Polygon } from 'react-google-maps';
+import HexbinComponent from './Hexbin.js';
+
+import fakeStoreLatLngData from './data/sample-data.json';
+
+const MAP_HEIGHT = 600;
+const HEX_PIXEL_RADIUS = 50;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Hexbin Demo</h2>
+        </div>
+        <GoogleMapLoader
+          query={{ libraries: 'geometry,drawing,places,visualization' }}
+          containerElement={
+            <div
+              style={{
+                width: '100%',
+                height: MAP_HEIGHT,
+              }}
+            />
+          }
+          googleMapElement={
+            <GoogleMap
+              style={{ border: '5px solid black' }}
+              ref={(map) => this._googleMapComponent = map}
+              defaultZoom={12}
+              options={{ mapTypeControl: false }}
+              defaultCenter={{ lat: 37.518397, lng: 126.978886 }}
+            >
+              <HexbinComponent
+                hexPixelRadius={HEX_PIXEL_RADIUS}
+                mapPixelHeight={MAP_HEIGHT}
+                data={fakeStoreLatLngData}
+                colorRange={['white', 'red']}
+              />
+            </GoogleMap>
+          }
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
